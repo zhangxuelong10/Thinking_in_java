420dc9baacc019bd0b2c229f015e26fcc1392121
zhangxuelong10 <xuelongzhang@vivedu.com>
2019-05-16 20:38:08 +0800

    ThreeSum

---
+++ src/main/java/algorithm/ThreeSum.java
@@ -0,0 +1,8 @@
+package algorithm;
+
+/**
+ * ThreeSum用于统计一个数组中和为0的三元组数量
+ */
+public interface ThreeSum {
+    int count(int[] nums);
+}
---
+++ src/main/java/algorithm/ThreeSumSlow.java
@@ -0,0 +1,9 @@
+package algorithm;
+
+public class ThreeSumSlow implements ThreeSum {
+
+    @Override
+    public int count(int[] nums) {
+        return 0;
+    }
+}
