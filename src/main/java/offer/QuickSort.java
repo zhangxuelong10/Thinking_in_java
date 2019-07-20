98e5f4b83c4fd0537512d4871a19018772cfce95
zhangxuelong10 <xuelongzhang@vivedu.com>
2019-07-21 00:23:27 +0800

    快排

---
+++ src/main/java/offer/QuickSort.java
@@ -0,0 +1,32 @@
+package offer;
+
+import static niuke.Code_00_BubbleSort.swap;
+
+public class QuickSort {
+    public static void quickSort(int[] arr, int L, int R) {
+        if (L < R) {
+            swap(arr, L + (int) (Math.random() * (R - L + 1)), R);
+            int[] p = partition(arr, L, R);
+            quickSort(arr, L, p[0] - 1);
+            quickSort(arr, p[1] + 1, R);
+        }
+    }
+
+    public static int[] partition(int[] arr, int L, int R) {
+        int less = L - 1;
+        int more = R;
+        while (L < more) {
+            if (arr[L] < arr[R]) {
+                swap(arr, ++less, L++);
+            } else if (arr[L] > arr[R]) {
+                swap(arr, --more, L);
+            } else {
+                L++;
+            }
+        }
+        swap(arr, more, R);
+        return new int[]{less + 1, more};
+    }
+
+    
+}
